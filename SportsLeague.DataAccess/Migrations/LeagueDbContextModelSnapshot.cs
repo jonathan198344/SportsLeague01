84f1909f423618423b08e1d0f8de0e37a735d486
jonathan198344 <jonathan_-1983@outlook.com>
2026-04-27 16:13:16 -0500

    model

--- SportsLeague.DataAccess/Migrations/LeagueDbContextModelSnapshot.cs
+++ SportsLeague.DataAccess/Migrations/LeagueDbContextModelSnapshot.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
 using SportsLeague.DataAccess.Context;
 
 #nullable disable
